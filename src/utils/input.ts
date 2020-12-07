adf42d590675ce6036d9cfbe1fc803a7a3ff43cd
Kees van Lierop <keesvanlierop@hotmail.com>
2020-12-08 00:47:20 +0100

    1a

---
+++ src/utils/input.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+
+const basePath = path.resolve(__dirname, '../input')
+
+export const readFile = (file: string, type = 'utf8'): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.resolve(basePath, file), type, (err, data) =>
+      err ? reject(err) : resolve(data),
+    )
+  })
+}
+
+export const readList = async (file: string) => {
+  const fileData = await readFile(file)
+  return fileData.split('\n')
+}
