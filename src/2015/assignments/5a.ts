b4e3f70d2cffcd17db84f46e0137a3f6517ddb28
Kees van Lierop <keesvanlierop@hotmail.com>
2021-12-05 23:31:26 +0100

    2015/5

---
+++ src/2015/assignments/5a.ts
@@ -0,0 +1,34 @@
+import { readList } from '@/utils'
+
+const defaultState = {
+	nice: [] as string[],
+	naughty: [] as string[],
+}
+
+const getStringState = (string: string[]) => {
+	return string.reduce(stringReducer, defaultState)
+}
+
+const stringReducer = (state: typeof defaultState, string: string) => {
+	const isNice = isNiceString(string)
+	const collectionToInsert = isNice ? 'nice' : 'naughty'
+
+	state[collectionToInsert].push(string)
+
+	return state
+}
+
+const isNiceString = (string: string) => {
+	return /(?:[aeiou][^aeiou]*){3}/.test(string) && /(.)\1+/.test(string) && !/ab|cd|pq|xy/.test(string)
+}
+
+const run = async () => {
+	const list = await readList('5.txt', '2015')
+
+	const stringState = getStringState(list)
+	console.log(stringState)
+
+	console.log(`The answer for 5a is: ${stringState.nice.length}`)
+}
+
+run()
---
+++ src/2015/assignments/5b.ts
@@ -0,0 +1,34 @@
+import { readList } from '@/utils'
+
+const defaultState = {
+	nice: [] as string[],
+	naughty: [] as string[],
+}
+
+const getStringState = (string: string[]) => {
+	return string.reduce(stringReducer, defaultState)
+}
+
+const stringReducer = (state: typeof defaultState, string: string) => {
+	const isNice = isNiceString(string)
+	const collectionToInsert = isNice ? 'nice' : 'naughty'
+
+	state[collectionToInsert].push(string)
+
+	return state
+}
+
+const isNiceString = (string: string) => {
+	return /(..).*\1/.test(string) && /(.).\1/.test(string)
+}
+
+const run = async () => {
+	const list = await readList('5.txt', '2015')
+
+	const stringState = getStringState(list)
+	console.log(stringState)
+
+	console.log(`The answer for 5b is: ${stringState.nice.length}`)
+}
+
+run()
