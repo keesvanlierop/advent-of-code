0bbd6dc42d5f56ab4ef9b81c3f6d63c53fad75c5
Kees van Lierop <keesvanlierop@hotmail.com>
2021-12-17 21:31:42 +0100

    2021/17

---
+++ src/2021/assignments/17b.ts
@@ -0,0 +1,82 @@
+import { readList } from '@/utils'
+
+const getTargetArea = (string: string) => {
+	const position = string.replace('target area: ', '')
+	const [x, y] = position.split(', ')
+
+	const [xMin, xMax] = x.replace('x=', '').split('..').map(Number)
+	const [yMin, yMax] = y.replace('y=', '').split('..').map(Number)
+
+	return {
+		x: {
+			min: xMin,
+			max: xMax,
+		},
+		y: {
+			min: yMin,
+			max: yMax,
+		},
+	}
+}
+
+const getValidProbes = (targetArea: ReturnType<typeof getTargetArea>) => {
+	let validProbes = []
+
+	for (let xVelocity = 0; xVelocity < targetArea.x.max * 2; xVelocity++) {
+		for (let yVelocity = 100; yVelocity > targetArea.y.min * 2; yVelocity--) {
+			const velocity = [xVelocity, yVelocity] as const
+
+			const probe = getProbe(targetArea, velocity)
+			if (probe.isValid) {
+				validProbes.push(probe)
+			}
+		}
+	}
+
+	return validProbes
+}
+
+const getProbe = (targetArea: ReturnType<typeof getTargetArea>, velocity: readonly [number, number]) => {
+	let isValid: boolean | null = null
+
+	const probePosition = [0, 0]
+	let iteration = 0
+	let journey = []
+
+	while (isValid === null) {
+		probePosition[0] = probePosition[0] + Math.max(0, velocity[0] - iteration)
+		probePosition[1] = probePosition[1] + velocity[1] - iteration
+
+		if (
+			probePosition[0] >= targetArea.x.min &&
+			probePosition[0] <= targetArea.x.max &&
+			probePosition[1] >= targetArea.y.min &&
+			probePosition[1] <= targetArea.y.max
+		)
+			isValid = true
+		else if (probePosition[0] > targetArea.x.max || probePosition[1] <= targetArea.y.min) isValid = false
+
+		journey.push([probePosition[0], probePosition[1]])
+
+		iteration++
+	}
+
+	return {
+		journey,
+		isValid,
+		velocity,
+	}
+}
+
+const run = async () => {
+	const list = await readList('17.txt')
+
+	const targetArea = getTargetArea(list[0])
+	const validProbes = getValidProbes(targetArea)
+
+	console.log(validProbes.length)
+
+	console.log(`The answer for 17b is: ${validProbes.length}`)
+}
+
+run()
