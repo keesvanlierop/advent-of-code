530d8b7db533d43de4d8c68035ed471c6e6c5fc2
Kees van Lierop <keesvanlierop@hotmail.com>
2021-12-17 23:04:08 +0100

    2015/11

---
+++ src/2015/assignments/11b.ts
@@ -0,0 +1,40 @@
+import { readList } from '@/utils'
+
+const isValidPassword = (password: string) => {
+	const charCodes = password.split('').map((c) => c.charCodeAt(0))
+	return (
+		charCodes.some((number, index, self) => self[index + 1] - number === 1 && self[index + 2] - self[index + 1] === 1) &&
+		!/(i|o|l)/.test(password) &&
+		/(\w)\1.*(\w)\2/.test(password)
+	)
+}
+
+// Increments one char
+const incrementChar = (char: string) => (char === 'z' ? 'a' : String.fromCharCode(char.charCodeAt(0) + 1))
+
+// Increments the whole string by one char recursively
+const increment = (string: string): string => {
+	const nextChar = incrementChar(string.slice(-1))
+	return nextChar === 'a' ? increment(string.slice(0, -1)) + 'a' : string.slice(0, -1) + nextChar
+}
+
+const getNextValidPassword = (input: string) => {
+	let currentPassword = input
+
+	while (!isValidPassword(currentPassword)) {
+		currentPassword = increment(currentPassword)
+	}
+
+	return currentPassword
+}
+
+const run = async () => {
+	const list = await readList('11.txt', '2015')
+
+	const password = getNextValidPassword(list[0])
+	const nextPassword = getNextValidPassword(increment(password))
+
+	console.log(`The answer for 11b is: ${nextPassword}`)
+}
+
+run()
